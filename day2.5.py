ebcb51706cee16a36819421e2bcea93f2227a022
Art38169 <86408487+Art38169@users.noreply.github.com>
2025-02-04 20:30:11 +0700

    animation.first

--- day2.5.py
+++ day2.5.py
@@ -15,15 +15,12 @@ count = 0
 for i in range(35):
     a = np.linspace(2, 4, 400)
     b = np.sin(a)
-
-    plt.plot(a, b)
-    plt.xlabel("x")
-    plt.ylabel("y")
     if f(low_x) * f(high_x) < 0:
         if f(low_x) * f(x_middle) < 0:
             high_x = x_middle
             x_middle = (low_x + high_x) / 2
             fig, ax = plt.subplots()
+            ax.plot(a, b)
             ax.plot(x_middle, f(x_middle), "ro")
             plt.savefig(f"bisect-demo-{i}.png")
             ax.clear
@@ -31,6 +28,7 @@ for i in range(35):
             low_x = x_middle
             x_middle = (low_x + high_x) / 2
             fig, ax = plt.subplots()
+            ax.plot(a, b)
             ax.plot(x_middle, f(x_middle), "ro")
             plt.savefig(f"bisect-demo-{i}.png")
             ax.clear
