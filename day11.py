6012b10c25147206cda49b12d9a1caa741f3b14c
Art38169 <86408487+Art38169@users.noreply.github.com>
2025-02-17 20:01:40 +0700

    line regression 1

--- day11.py
+++ day11.py
@@ -1,25 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
+import random
 
 
-def F(x):
-    return 1.0/(1+ 25 * x ** 2)
 
-x = np.linspace(-1, 1, 20)
-y = F(x)
+c0 = 1
+c1 = 1
+a = 0
+b = 10
+m = 100
 
-def plot_spline(x, y):
-    cs = CubicSpline(x, y, bc_type= 'natural')
-    
-    x_new = np.linspace(-1, 1, 100)
-    y_new = cs(x_new)
-    
-    plt.figure(figsize = (8, 8))
-    plt.plot(x_new, y_new, color = 'orange', label = 'Natural Cubic Spline')
-    plt.scatter(x, y, color = 'blue', label = 'Data Points')
-    plt.legend()
-    plt.show()
-    
-plot_spline(x, y)
-        
+def f(x):
+    return c0 + c1 * x
+
+x_points = np.linspace(a, b, m)
+y_points = f(x_points)
+y_points_noisy = y_points + np.random.rand(m) - 0.5
+
+A = np.zeros((2, 2))
+B = np.zeros(2)
+
+A[0, 0] = m
+A[0, 1] = np.sum(x_points)
+A[1, 0] = np.sum(x_points)
+A[1, 1] = np.sum(x_points ** 2)
+
+B[0] = np.sum(y_points_noisy)
+B[1] = np.sum(x_points * y_points_noisy)
+
+c = np.linalg.solve(A, B)
+
+def P(x, c):
+    return c[0] + c[1] * x
+
+x_fine = np.linspace(a, b, 200)
+f_value = f(x_fine)
+p_values = P(x_fine, c)
+
+plt.figure(figsize = (8, 6))
+plt.plot(x_fine, f_value, label = 'f(x)', color = 'blue')
+plt.plot(x_fine, p_values, label = 'P(x)', color = 'red', linestyle = '--')
+plt.scatter(x_points, y_points_noisy, label = 'data points')
+plt.legend()
+plt.show()
