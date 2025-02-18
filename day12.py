f2e601f8685323c683f57900de2d2a20300ccdf3
Art38169 <86408487+Art38169@users.noreply.github.com>
2025-02-18 19:39:50 +0700

    classwork about coefficient

--- day12.py
+++ day12.py
@@ -1,17 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def g(x, c):
-    return c[0] + c[1] * np.sin(x) + c[2] * x ** 2
 
-A = np.array([[1, 0, 0], [1, np.sin(1), 1], [1, np.sin(2), 4], [1, np.sin(3), 9]])
-b = np.array([1, 2, 3, 2])
+def f1(t):
+    return np.cos(t)
+
+def f2(t):
+    return np.sin(t)
+
+def f3(t):
+    return t
+
+def f4(t):
+    return t ** 2
+
+def f5(t):
+    return np.log(t)
+
+def g(x, c):
+    return c[0] + c[1] * f1(x) + c[2] * f2(x) + c[3] * f3(x) + c[4] * f4(x) + c[5] * f5(x)
+
+x = np.loadtxt("data[1].csv", delimiter= ',', usecols= 0)
+y = np.loadtxt("data[1].csv", delimiter= ',', usecols= 1)
+
+N = 500
+A = np.zeros((N, 6))
+b = np.zeros(N)
+
+for i in range(N):
+    A[i, 0] = 1
+    A[i, 1] = f1(x[i])
+    A[i, 2] = f2(x[i])
+    A[i, 3] = f3(x[i])
+    A[i, 4] = f4(x[i])
+    A[i, 5] = f5(x[i])
+    b[i] = y[i]
 
 c = np.linalg.solve(A.T@A, A.T@b)
+print(f"c is: {c}")
 
-print(f"Corrupted Coefficient: {c}")
-
-x_fine = np.linspace(0, 3, 400)
+x_fine = np.linspace(1, 5, 1000)
 g_values = g(x_fine, c)
-plt.plot(x_fine, g_values, label = "graphs")
+plt.plot(x_fine, g_values, color = 'orange', label = 'Regression Line')
+plt.scatter(x, y, label = 'Data Points')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
 plt.show()
+
+
+        
